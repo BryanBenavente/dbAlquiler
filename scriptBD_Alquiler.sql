f712f74d938b4baf3a37a9edecb04a47cc741a2e
BryanBenavente <bryan.benavente@vallegrande.edu.pe>
2018-11-26 09:38:49 -0500

    Creacion de la tabla PERSONA

--- scriptBD_Alquiler.sql
+++ scriptBD_Alquiler.sql
@@ -10,8 +10,23 @@ CREATE DATABASE dbAlquiler DEFAULT CHARACTER set utf8;
 use dbAlquiler;
 
 /* Verificar que la BD este en uso */
-select DATABASE();
+SELECT DATABASE();
 
-create table Persona(
-    NOMPER varchar(50)
+create table PERSONA(
+    CODPER int,
+    NOMPER varchar(100),
+    APEPATPER VARCHAR(150),
+    APEMATPER VARCHAR(150),
+    DNIPER CHAR(8),
+    DIRPER VARCHAR(150),
+    UBIGEO_CODUBI CHAR(6),
+    TELEF1PER CHAR(9),
+    TELEF2PER CHAR(9),
+    SEXOPER CHAR(1),
+    TIPOPER CHAR(1),
+    USUPER VARCHAR(50),
+    PASSPER VARCHAR(50),
+    CONSTRAINT CODPER_PK PRIMARY KEY(CODPER)
 );
+
+SHOW COLUMNS IN PERSONA;
