033264211b0b96119ddc7af1f67564669cbf76bd
BryanBenavente <bryan.benavente@vallegrande.edu.pe>
2018-11-26 09:23:53 -0500

    Creacion de la BD Alquiler

--- scriptBD_Alquiler.sql
+++ scriptBD_Alquiler.sql
@@ -1,6 +1,17 @@
 /* Listar BD del servidor */
 show DATABASES;
 
-/* eliminar BD de datos*/
+/* eliminar BD de datos */
 DROP DATABASE dbAlquiler;
 
+/* Crear BD */
+CREATE DATABASE dbAlquiler DEFAULT CHARACTER set utf8;
+
+use dbAlquiler;
+
+/* Verificar que la BD este en uso */
+select DATABASE();
+
+create table Persona(
+    NOMPER varchar(50)
+);
